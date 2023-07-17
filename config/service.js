fa95b5f16ed4be1dd1bddecc4a6e76a7d5350b82
Abhishek Bhardwaj <abhishek.hcst14@gmail.com>
2023-07-17 21:32:30 +0530

    Update service.js
    Updating mongodb connection string

--- config/service.js
+++ config/service.js
@@ -1,8 +1,8 @@
 module.exports = {
-  account_database_uri: 'mongodb+srv://<username>:<password>@<mongo-cluster-url>/tiq-<instance-id>',
-  master_database_uri: 'mongodb+srv://<username>:<password>@<mongo-cluster-url>/tiq-master-<instance-id>',
+  account_database_uri: 'mongodb+srv://cgabhishek:A9mFYNBRtiFj5xKF@cgabhishekcluster.vnzb7o4.mongodb.net/cb-10000',
+  master_database_uri: 'mongodb+srv://cgabhishek:A9mFYNBRtiFj5xKF@cgabhishekcluster.vnzb7o4.mongodb.net/cb-master-10000',
   event_database_uri: 'mongodb+srv://<username>:<password>@<mongo-cluster-url>/tiq-event-<instance-id>',
-  public_database_uri: "mongodb+srv://cgabhishek:A9mFYNBRtiFj5xKF@cgabhishekcluster.vnzb7o4.mongodb.net/",
+  public_database_uri: 'mongodb+srv://cgabhishek:A9mFYNBRtiFj5xKF@cgabhishekcluster.vnzb7o4.mongodb.net/',
 
   secret: 'Talent1Q$ecret',
   guid_format: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx',
