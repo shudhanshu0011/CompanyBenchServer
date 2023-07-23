dd920d4aa4db51c43dc0920e300cfe442e26f406
Abhishek Bhardwaj <abhishek.hcst14@gmail.com>
2023-07-23 23:54:51 +0530

    user entity added

---
+++ config/basicInfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+    openapi: "3.0.3", // present supported openapi version
+    info: {
+      title: "Simple Todos API", // short title.
+      description: "A simple todos API", //  desc.
+      version: "1.0.0", // version number
+      contact: {
+        name: "Abhishek B", // your name
+        email: "cgabhishek@outlook.cpm", // your email
+        url: "Companybench.com", // your website
+      },
+    },
+  };
--- config/error_codes.js
+++ config/error_codes.js
@@ -1,8 +1,14 @@
 module.exports = {
     customer: {
-        create_failed: {code: 'TIQ-Customer-001', message: 'Failed to Save New Customer'},
-        update_failed: {code: 'TIQ-Customer-002', message: 'Failed to Update Customer'},
-        read_all_failed: {code: 'TIQ-Customer-003', message: 'Failed fetch Customer(s)'},
-        read_failed: {code: 'TIQ-Customer-004', message: 'Failed fetch Customer'},
+        create_failed: {code: 'CB-Customer-001', message: 'Failed to Save New Customer'},
+        update_failed: {code: 'CB-Customer-002', message: 'Failed to Update Customer'},
+        read_all_failed: {code: 'CB-Customer-003', message: 'Failed fetch Customer(s)'},
+        read_failed: {code: 'CB-Customer-004', message: 'Failed fetch Customer'},
+      },
+      user: {
+        create_failed: {code: 'CB-user-001', message: 'Failed to Save New user'},
+        update_failed: {code: 'CB-user-002', message: 'Failed to Update user'},
+        read_all_failed: {code: 'CB-user-003', message: 'Failed fetch user(s)'},
+        read_failed: {code: 'CB-user-004', message: 'Failed fetch user'},
       }
 };
