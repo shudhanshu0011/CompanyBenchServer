099da146e0f794c8a30538c18b300b8e946b9bf9
Prajjwal Laad <prajjwal.laad@gmail.com>
2023-09-24 18:18:10 +0530

    Add Localhost to cors allowed origins

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ const mongoose = require("mongoose");
 
 //  Express App setup
 const app = express();
-app.use(cors());
+// TODO: Populate from service config
+const whitelist = ['http://localhost:8080', 'http://example2.com'];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin))
+      return callback(null, true)
+
+      callback(new Error('Not allowed by CORS'));
+  }
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // AWS and MongoDB connection
